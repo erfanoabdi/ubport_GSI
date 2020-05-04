43a0964efb6cd12edb173507c5b59e215184c995
Erfan Abdi <erfangplus@gmail.com>
2020-05-04 08:21:28 +0430

    Assume "yes" to all e2fsck questions

--- ubports.sh
+++ ubports.sh
@@ -32,7 +32,7 @@ umount /r;
 mv /data/system.img /data/android-rootfs.img
 
 ui_print "Resizing rootfs to 8GB";
-e2fsck -f /data/rootfs.img
+e2fsck -fy /data/rootfs.img
 resize2fs /data/rootfs.img 8G
 
 ## end install
